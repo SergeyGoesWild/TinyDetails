7e6650287986a221e28d34bf2f58a27b44342e78
Sergey Telnov <sergej.telnov@gmail.com>
2025-03-16 16:54:50 +0100

    First commit + Splash screen

---
+++ TinyDetails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinyDetails
+//
+//  Created by Sergey Telnov on 16/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
