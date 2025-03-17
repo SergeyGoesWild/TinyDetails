bb68bc11e68830976d3147bae0fcfa670ea1b9bc
Sergey Telnov <sergej.telnov@gmail.com>
2025-03-18 00:29:33 +0100

    Setting the right size of the image, WIP

--- TinyDetails/ViewController.swift
+++ TinyDetails/ViewController.swift
@@ -9,13 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     var bgSolid: UIView!
-    var imagePH: UIView!
+    var imagePH: UIImageView!
     var clickable1: UIView!
     var clickable2: UIView!
     var clickable3: UIView!
     var centerCommon: UIView!
     var centerScroll: UIView!
     var scrollView: UIScrollView!
+    var image: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,8 +33,9 @@ class ViewController: UIViewController {
         scrollView.showsHorizontalScrollIndicator = true
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         
-        imagePH = UIView()
-        imagePH.backgroundColor = UIColor(red: 1.00, green: 0.62, blue: 0.26, alpha: 1.00)
+        image = UIImage(named: "BigImage")
+        imagePH = UIImageView(image: image)
+//        imagePH.backgroundColor = UIColor(red: 1.00, green: 0.62, blue: 0.26, alpha: 1.00)
         imagePH.translatesAutoresizingMaskIntoConstraints = false
         
         centerCommon = UIView()
@@ -61,8 +63,8 @@ class ViewController: UIViewController {
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             
-            imagePH.widthAnchor.constraint(equalToConstant: 1000),
-            imagePH.heightAnchor.constraint(equalToConstant: 600),
+            imagePH.widthAnchor.constraint(equalToConstant: image.size.width),
+            imagePH.heightAnchor.constraint(equalToConstant: image.size.height),
             imagePH.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
             imagePH.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
             
@@ -80,6 +82,7 @@ class ViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
         view.layoutIfNeeded()
         scrollView.contentSize = CGSize(width: imagePH.bounds.width, height: imagePH.bounds.height)
         let centerX = (scrollView.contentSize.width - scrollView.bounds.width) / 2
@@ -89,17 +92,28 @@ class ViewController: UIViewController {
         clickable1 = UIView()
         clickable1.backgroundColor = UIColor(red: 0.13, green: 0.18, blue: 0.24, alpha: 1.00)
         clickable1.translatesAutoresizingMaskIntoConstraints = false
+        let gesture1 = UITapGestureRecognizer(target: self, action: #selector(onClick))
+        clickable1.addGestureRecognizer(gesture1)
+        clickable1.isUserInteractionEnabled = true
         imagePH.addSubview(clickable1)
         
         clickable2 = UIView()
         clickable2.backgroundColor = UIColor(red: 0.13, green: 0.18, blue: 0.24, alpha: 1.00)
         clickable2.translatesAutoresizingMaskIntoConstraints = false
+//        clickable2.addGestureRecognizer(gesture1)
+        clickable2.isUserInteractionEnabled = true
         imagePH.addSubview(clickable2)
         
         clickable3 = UIView()
         clickable3.backgroundColor = UIColor(red: 0.13, green: 0.18, blue: 0.24, alpha: 1.00)
         clickable3.translatesAutoresizingMaskIntoConstraints = false
+//        clickable3.addGestureRecognizer(gesture1)
+        clickable3.isUserInteractionEnabled = true
         imagePH.addSubview(clickable3)
+        
+        let imageViewSize = getImageSize(image: image)
+        imagePH.frame.size = imageViewSize
+        print(imageViewSize)
         
         NSLayoutConstraint.activate([
             clickable1.widthAnchor.constraint(equalToConstant: 100),
@@ -119,8 +133,22 @@ class ViewController: UIViewController {
         ])
     }
     
-    @objc func onClick(number: Int) {
+    @objc func onClick() {
         print("CLICKED!")
+    }
+    
+    func getImageSize(image: UIImage) -> CGSize {
+        if image.size.width > image.size.height {
+            let multiplier = image.size.height / scrollView.frame.height
+            let height = scrollView.frame.height
+            let width = image.size.width / multiplier
+            return CGSize(width: width, height: height)
+        } else {
+            let multiplier = image.size.width / scrollView.frame.width
+            let height = image.size.height / multiplier
+            let width = scrollView.frame.width
+            return CGSize(width: width, height: height)
+        }
     }
 }
 
