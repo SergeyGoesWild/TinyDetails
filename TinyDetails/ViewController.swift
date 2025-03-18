dd6027c2241e7d74a0c22f5421fae800694c5839
Sergey Telnov <sergej.telnov@gmail.com>
2025-03-18 22:03:14 +0100

    The image is set the right way now, prototyping WIP. Debugging the clickable zones.

--- TinyDetails/ViewController.swift
+++ TinyDetails/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let clickAreaDiam = 100
+    
     var bgSolid: UIView!
     var imagePH: UIImageView!
     var clickable1: UIView!
@@ -33,9 +35,8 @@ class ViewController: UIViewController {
         scrollView.showsHorizontalScrollIndicator = true
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         
-        image = UIImage(named: "BigImage")
+        image = UIImage(named: "SmallImage")
         imagePH = UIImageView(image: image)
-//        imagePH.backgroundColor = UIColor(red: 1.00, green: 0.62, blue: 0.26, alpha: 1.00)
         imagePH.translatesAutoresizingMaskIntoConstraints = false
         
         centerCommon = UIView()
@@ -46,9 +47,24 @@ class ViewController: UIViewController {
         centerScroll.backgroundColor = .white
         centerScroll.translatesAutoresizingMaskIntoConstraints = false
         
+        clickable1 = UIView()
+        clickable1.backgroundColor = UIColor(red: 0.13, green: 0.18, blue: 0.24, alpha: 1.00)
+        clickable1.translatesAutoresizingMaskIntoConstraints = false
+        let gesture1 = UITapGestureRecognizer(target: self, action: #selector(onClick))
+        clickable1.addGestureRecognizer(gesture1)
+        clickable1.isUserInteractionEnabled = true
+        
+        clickable2 = UIView()
+        clickable2.backgroundColor = UIColor(red: 0.13, green: 0.18, blue: 0.24, alpha: 1.00)
+        clickable2.translatesAutoresizingMaskIntoConstraints = false
+        clickable2.addGestureRecognizer(gesture1)
+        clickable2.isUserInteractionEnabled = true
+        
         view.addSubview(bgSolid)
         view.addSubview(scrollView)
         scrollView.addSubview(imagePH)
+        imagePH.addSubview(clickable1)
+        imagePH.addSubview(clickable2)
         imagePH.addSubview(centerScroll)
         view.addSubview(centerCommon)
         
@@ -63,73 +79,46 @@ class ViewController: UIViewController {
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             
-            imagePH.widthAnchor.constraint(equalToConstant: image.size.width),
-            imagePH.heightAnchor.constraint(equalToConstant: image.size.height),
-            imagePH.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
-            imagePH.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
-            
             centerCommon.widthAnchor.constraint(equalToConstant: 25),
             centerCommon.heightAnchor.constraint(equalToConstant: 25),
             centerCommon.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             centerCommon.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
-            
-            centerScroll.widthAnchor.constraint(equalToConstant: 40),
-            centerScroll.heightAnchor.constraint(equalToConstant: 40),
-            centerScroll.centerXAnchor.constraint(equalTo: imagePH.centerXAnchor),
-            centerScroll.centerYAnchor.constraint(equalTo: imagePH.centerYAnchor),
         ])
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        let imageViewSize = getImageSize(image: image, scrollView: scrollView)
         
-        view.layoutIfNeeded()
-        scrollView.contentSize = CGSize(width: imagePH.bounds.width, height: imagePH.bounds.height)
+        if scrollView.contentSize != CGSize(width: imageViewSize.width, height: imageViewSize.height) {
+            scrollView.contentSize = CGSize(width: imageViewSize.width, height: imageViewSize.height)
+        }
+        
         let centerX = (scrollView.contentSize.width - scrollView.bounds.width) / 2
         let centerY = (scrollView.contentSize.height - scrollView.bounds.height) / 2
         scrollView.setContentOffset(CGPoint(x: centerX, y: centerY), animated: false)
         
-        clickable1 = UIView()
-        clickable1.backgroundColor = UIColor(red: 0.13, green: 0.18, blue: 0.24, alpha: 1.00)
-        clickable1.translatesAutoresizingMaskIntoConstraints = false
-        let gesture1 = UITapGestureRecognizer(target: self, action: #selector(onClick))
-        clickable1.addGestureRecognizer(gesture1)
-        clickable1.isUserInteractionEnabled = true
-        imagePH.addSubview(clickable1)
-        
-        clickable2 = UIView()
-        clickable2.backgroundColor = UIColor(red: 0.13, green: 0.18, blue: 0.24, alpha: 1.00)
-        clickable2.translatesAutoresizingMaskIntoConstraints = false
-//        clickable2.addGestureRecognizer(gesture1)
-        clickable2.isUserInteractionEnabled = true
-        imagePH.addSubview(clickable2)
-        
-        clickable3 = UIView()
-        clickable3.backgroundColor = UIColor(red: 0.13, green: 0.18, blue: 0.24, alpha: 1.00)
-        clickable3.translatesAutoresizingMaskIntoConstraints = false
-//        clickable3.addGestureRecognizer(gesture1)
-        clickable3.isUserInteractionEnabled = true
-        imagePH.addSubview(clickable3)
-        
-        let imageViewSize = getImageSize(image: image)
-        imagePH.frame.size = imageViewSize
-        print(imageViewSize)
-        
         NSLayoutConstraint.activate([
-            clickable1.widthAnchor.constraint(equalToConstant: 100),
-            clickable1.heightAnchor.constraint(equalToConstant: 100),
-            clickable1.leadingAnchor.constraint(equalTo: imagePH.leadingAnchor, constant: imagePH.bounds.width * 0.1),
-            clickable1.topAnchor.constraint(equalTo: imagePH.topAnchor, constant: imagePH.bounds.height * 0.1),
             
-            clickable2.widthAnchor.constraint(equalToConstant: 100),
-            clickable2.heightAnchor.constraint(equalToConstant: 100),
-            clickable2.leadingAnchor.constraint(equalTo: imagePH.leadingAnchor, constant: imagePH.bounds.width * 0.4),
-            clickable2.topAnchor.constraint(equalTo: imagePH.topAnchor, constant: imagePH.bounds.height * 0.7),
+            imagePH.widthAnchor.constraint(equalToConstant: imageViewSize.width),
+            imagePH.heightAnchor.constraint(equalToConstant: imageViewSize.height),
+            imagePH.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            imagePH.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
             
-            clickable3.widthAnchor.constraint(equalToConstant: 100),
-            clickable3.heightAnchor.constraint(equalToConstant: 100),
-            clickable3.leadingAnchor.constraint(equalTo: imagePH.leadingAnchor, constant: imagePH.bounds.width * 0.85),
-            clickable3.topAnchor.constraint(equalTo: imagePH.topAnchor, constant: imagePH.bounds.height * 0.85)
+            centerScroll.widthAnchor.constraint(equalToConstant: 40),
+            centerScroll.heightAnchor.constraint(equalToConstant: 40),
+            centerScroll.centerXAnchor.constraint(equalTo: imagePH.centerXAnchor),
+            centerScroll.centerYAnchor.constraint(equalTo: imagePH.centerYAnchor),
+            
+            clickable1.widthAnchor.constraint(equalToConstant: CGFloat(clickAreaDiam)),
+            clickable1.heightAnchor.constraint(equalToConstant: CGFloat(clickAreaDiam)),
+            clickable1.leadingAnchor.constraint(equalTo: imagePH.leadingAnchor, constant: imageViewSize.width * 0.5 - CGFloat(clickAreaDiam / 2)),
+            clickable1.topAnchor.constraint(equalTo: imagePH.topAnchor, constant: imageViewSize.height * 0.5 - CGFloat(clickAreaDiam / 2)),
+            
+            clickable2.widthAnchor.constraint(equalToConstant: CGFloat(clickAreaDiam)),
+            clickable2.heightAnchor.constraint(equalToConstant: CGFloat(clickAreaDiam)),
+            clickable2.leadingAnchor.constraint(equalTo: imagePH.leadingAnchor, constant: imageViewSize.width * 0.4 - CGFloat(clickAreaDiam / 2)),
+            clickable2.topAnchor.constraint(equalTo: imagePH.topAnchor, constant: imageViewSize.height * 0.7 - CGFloat(clickAreaDiam / 2)),
         ])
     }
     
@@ -137,18 +126,13 @@ class ViewController: UIViewController {
         print("CLICKED!")
     }
     
-    func getImageSize(image: UIImage) -> CGSize {
-        if image.size.width > image.size.height {
-            let multiplier = image.size.height / scrollView.frame.height
-            let height = scrollView.frame.height
-            let width = image.size.width / multiplier
-            return CGSize(width: width, height: height)
-        } else {
-            let multiplier = image.size.width / scrollView.frame.width
-            let height = image.size.height / multiplier
-            let width = scrollView.frame.width
-            return CGSize(width: width, height: height)
-        }
+    func getImageSize(image: UIImage, scrollView: UIScrollView) -> CGSize {
+        guard scrollView.frame.height > 0 else { return image.size }
+        
+        let multiplier = image.size.height / scrollView.frame.height
+        let height = scrollView.frame.height
+        let width = image.size.width / multiplier
+        return CGSize(width: width, height: height)
     }
 }
 
