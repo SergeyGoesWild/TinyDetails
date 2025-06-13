a198f3ea695eac289a8a9c41229dde381087e5aa
Sergey Telnov <sergej.telnov@gmail.com>
2025-06-13 20:39:57 +0200

    Fixing the code to match with the new design. Main logic changed, fixing bugs now.

--- TinyDetails/ViewController.swift
+++ TinyDetails/ViewController.swift
@@ -17,18 +17,29 @@ protocol EndLevelDelegate: AnyObject {
 
 class ViewController: UIViewController {
     static let paintingList = DataProvider.shared.paintingList
-    var resultModel: [ResultObject] = []
+
+    var currentLevelIndex: Int = 0
+    var currentItemIndex: Int = -1
     
-    var hintTimer: Timer?
+    lazy var currentLevel: PaintingObject = {
+        return ViewController.paintingList[currentItemIndex]
+    }()
+    
+    lazy var currentItem: ClickableArea = {
+        return ViewController.paintingList[currentItemIndex].areas[currentItemIndex]
+    }()
+    
+    lazy var levelIsOver: Bool = {
+        return currentItemIndex <= currentLevel.areas.count
+    }()
     
     var imageViewSize: CGSize!
-    var redrawing: Bool = true
+    var isFirstLaunch: Bool = true
     var inTransition: Bool = false
     
     let sideMargin: CGFloat = 16
     let collectionInsets = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
     
-    var currentLevel: Int = 0
     var widthConstraintImagePH: NSLayoutConstraint!
     var heightConstraintImagePH: NSLayoutConstraint!
     
@@ -43,71 +54,55 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("eeeeeeee")
-        setupResultModel()
         setupNormalLayout()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        print("REDRAWING")
-        if redrawing {
-            setupPictureLayout(currentLevel: currentLevel)
-            setupClickableAreas()
+        print("in ViewDidLayoutSubviews")
+        if isFirstLaunch {
+            setupPictureLayout(currentLevel: ViewController.paintingList[0])
+            setNextItem()
         }
-        redrawing = false
+        isFirstLaunch = false
     }
     
-    private func getImageSize(image: UIImage, scrollView: UIScrollView) -> CGSize {
-        guard scrollView.frame.height > 0 else { return image.size }
-        
-        let multiplier = image.size.height / scrollView.frame.height
-        let height = scrollView.frame.height
-        let width = image.size.width / multiplier
-        return CGSize(width: width, height: height)
+    // MARK: - Flow
+    
+    private func setNextItem() {
+        print("in setting NEXT ITEM")
+        currentItemIndex += 1
+        itemTextLabel.text = currentItem.hintText
+        setupClickableArea(areaData: currentItem)
+        view.layoutIfNeeded()
     }
     
     private func checkLevelComplete() {
-        for item in resultModel {
-            if !item.wasClicked {
-                return
-            }
-        }
-        let endLevelScreen = EndLevelScreen(paintingObject: ViewController.paintingList[currentLevel], delegate: self)
-        endLevelScreen.modalPresentationStyle = .automatic
-        // TODO: Find a more elegant solution
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.present(endLevelScreen, animated: true, completion: nil)
+        print("in CHECK")
+        if levelIsOver {
+            changeLevel()
+        } else {
+            setNextItem()
         }
     }
     
     @objc private func changeLevel() {
-        currentLevel += 1
-        resultModel = []
-        setupResultModel()
+        currentLevelIndex += 1
+        currentItemIndex = -1
         // TODO: TRASH, CHANGE THAT, THE ANIMATION PREVENTS CONTROL
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             self.setupPictureLayout(currentLevel: self.currentLevel)
-            self.setupClickableAreas()
+            self.setNextItem()
             self.scrollView.zoomScale = 1
         }
     }
     
-    private func setupResultModel() {
-        for area in ViewController.paintingList[currentLevel].areas {
-            let newObject = ResultObject(id: area.idArea, title: area.hintText, wasClicked: false)
-            resultModel.append(newObject)
-        }
-    }
+    // MARK: - Layout
     
     private func setupNormalLayout() {
-        
         bgSolid = UIView()
         bgSolid.backgroundColor = UIColor(red: 0.18, green: 0.53, blue: 0.87, alpha: 1.00)
         bgSolid.translatesAutoresizingMaskIntoConstraints = false
-        
-//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapOnHint))
-//        hintTouchField.addGestureRecognizer(tapGesture)
         
         scrollView = UIScrollView()
         scrollView.delegate = self
@@ -132,19 +127,18 @@ class ViewController: UIViewController {
         widthConstraintImagePH = imagePH.widthAnchor.constraint(equalToConstant: 0)
         heightConstraintImagePH = imagePH.heightAnchor.constraint(equalToConstant: 0)
         
-        itemTextLabel = UILabel()
-        itemTextLabel.text = "Adam?"
-        itemTextLabel.textColor = .white
-        itemTextLabel.font = UIFont.systemFont(ofSize: 55, weight: .bold)
-        itemTextLabel.textAlignment = .left
-        itemTextLabel.translatesAutoresizingMaskIntoConstraints = false
-        
         questionTextLabel = UILabel()
         questionTextLabel.text = "Can you find"
         questionTextLabel.textColor = .white
         questionTextLabel.font = UIFont.systemFont(ofSize: 40, weight: .medium)
         questionTextLabel.textAlignment = .left
         questionTextLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        itemTextLabel = UILabel()
+        itemTextLabel.textColor = .white
+        itemTextLabel.font = UIFont.systemFont(ofSize: 55, weight: .bold)
+        itemTextLabel.textAlignment = .left
+        itemTextLabel.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(bgSolid)
         view.addSubview(scrollView)
@@ -179,26 +173,12 @@ class ViewController: UIViewController {
         ])
     }
     
-    private func setupClickableArea(areaData: ClickableArea) {
-        let area = ClickableAreaView(id: areaData.idArea)
-        area.delegate = self
-        area.translatesAutoresizingMaskIntoConstraints = false
-        imagePH.addSubview(area)
-        NSLayoutConstraint.activate([
-            area.widthAnchor.constraint(equalToConstant: areaData.size),
-            area.heightAnchor.constraint(equalToConstant: areaData.size),
-            area.leadingAnchor.constraint(equalTo: imagePH.leadingAnchor, constant: imageViewSize.width * (areaData.xPercent / 100) - CGFloat(areaData.size / 2)),
-            area.topAnchor.constraint(equalTo: imagePH.topAnchor, constant: imageViewSize.height * (areaData.yPercent / 100) - CGFloat(areaData.size / 2)),
-        ])
-        imagePH.bringSubviewToFront(area)
-    }
-    
-    private func setupPictureLayout(currentLevel: Int) {
-        for area in imagePH.subviews.compactMap({ $0 as? ClickableAreaView }) {
-            area.removeFromSuperview()
-        }
+    private func setupPictureLayout(currentLevel: PaintingObject) {
+//        for area in imagePH.subviews.compactMap({ $0 as? ClickableAreaView }) {
+//            area.removeFromSuperview()
+//        }
         
-        image = UIImage(named: ViewController.paintingList[currentLevel].paintingFile)
+        image = UIImage(named: currentLevel.paintingFile)
         imagePH.image = image
         
         imageViewSize = getImageSize(image: image, scrollView: scrollView)
@@ -217,28 +197,40 @@ class ViewController: UIViewController {
         scrollView.setContentOffset(CGPoint(x: centerX, y: centerY), animated: false)
     }
     
-    func findCellWithID(_ id: UUID) {
-        guard let index = ViewController.paintingList[currentLevel].areas.firstIndex(where: { $0.idArea == id }) else {
-            return
-        }
-        let indexPath = IndexPath(item: index, section: 0)
-        if let resultIndex = resultModel.firstIndex(where: {$0.id == id}) {
-            resultModel[resultIndex].wasClicked = true
-//            collectionView.reloadItems(at: [indexPath])
-            checkLevelComplete()
-        }
+    // MARK: - Service
+    
+    private func getImageSize(image: UIImage, scrollView: UIScrollView) -> CGSize {
+        guard scrollView.frame.height > 0 else { return image.size }
+        
+        let multiplier = image.size.height / scrollView.frame.height
+        let height = scrollView.frame.height
+        let width = image.size.width / multiplier
+        return CGSize(width: width, height: height)
     }
     
-    func setupClickableAreas() {
-        for areaItem in ViewController.paintingList[currentLevel].areas {
-            setupClickableArea(areaData: areaItem)
-        }
+    private func setupClickableArea(areaData: ClickableArea) {
+        let area = ClickableAreaView(id: areaData.idArea)
+        area.delegate = self
+        area.translatesAutoresizingMaskIntoConstraints = false
+        imagePH.addSubview(area)
+        NSLayoutConstraint.activate([
+            area.widthAnchor.constraint(equalToConstant: areaData.size),
+            area.heightAnchor.constraint(equalToConstant: areaData.size),
+            area.leadingAnchor.constraint(equalTo: imagePH.leadingAnchor, constant: imageViewSize.width * (areaData.xPercent / 100) - CGFloat(areaData.size / 2)),
+            area.topAnchor.constraint(equalTo: imagePH.topAnchor, constant: imageViewSize.height * (areaData.yPercent / 100) - CGFloat(areaData.size / 2)),
+        ])
+        imagePH.bringSubviewToFront(area)
+    }
+    
+    private func launchRightGuessAnimation() {
+        
     }
 }
 
 extension ViewController: ClickableAreaDelegate {
     func didReceiveClick(area: ClickableAreaView) {
-        findCellWithID(area.areaID)
+        launchRightGuessAnimation()
+        checkLevelComplete()
     }
 }
 
