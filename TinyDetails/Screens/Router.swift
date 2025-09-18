3c8d7ea8befcb97ec6a102c3a92b453cb1b94dcf
Sergey Telnov <sergej.telnov@gmail.com>
2025-09-18 18:58:11 +0200

    Removed comments.

--- TinyDetails/Screens/Router.swift
+++ TinyDetails/Screens/Router.swift
@@ -51,39 +51,4 @@ final class Router: RouterProtocol {
     func popToRoot(animated: Bool = true) {
         nav?.popToRootViewController(animated: animated)
     }
-    
-//    private func make(route: Route) -> UIViewController {
-//        switch route {
-//        case .screen1:
-//            return ModuleBuilder.makeScreen1(router: self, services: services)
-//            
-//        case .screen2(let input):
-//            return ModuleBuilder.makeScreen2(input: input, router: self, services: services)
-//            
-//        case .screen3:
-//            return ModuleBuilder.makeScreen3(router: self, services: services)
-//        }
-//    }
-    
-    //    override func viewDidLoad() {
-    //        super.viewDidLoad()
-    //        setupGameComponent()
-    //    }
-    //
-    //    private func setupGameComponent() {
-    //        navController = UINavigationController(rootViewController: gameVC)
-    //        navController.setNavigationBarHidden(true, animated: false)
-    //        addChild(navController)
-    //        view.addSubview(navController.view)
-    //        navController.view.translatesAutoresizingMaskIntoConstraints = false
-    //
-    //        NSLayoutConstraint.activate([
-    //            navController.view.topAnchor.constraint(equalTo: view.topAnchor),
-    //            navController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-    //            navController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-    //            navController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-    //        ])
-    //
-    //        navController.didMove(toParent: self)
-    //    }
 }
